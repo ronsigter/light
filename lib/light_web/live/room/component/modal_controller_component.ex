226ec317d25803bd3c69483314fe200fd3c91a14
ronsigter <ronchester.mendoza@gmail.com>
2020-08-29 20:01:42 +0800

    adds a component

---
+++ lib/light_web/live/room/component/modal_controller_component.ex
@@ -0,0 +1,93 @@
+defmodule LightWeb.Room.Component.ModalController do
+  use Phoenix.LiveComponent
+
+  def render(assigns) do
+    ~L"""
+    <h1><%= @room.name %> <%= @room.brightness %></h1>
+    <div class="controller-container" id="<%= @id %>">
+      <div class="switch">
+        <button phx-click="switch" phx-value-switch="off" phx-target="<%= @id %>">Off</button>
+        <button phx-click="switch" phx-value-switch="on" phx-target="<%= @id %>">On</button>
+      </div>
+      <div class="dimmers">
+        <button phx-click="dimmer" phx-value-dimmer="down" phx-target="<%= @id %>">Down</button>
+        <button phx-click="dimmer" phx-value-dimmer="up" phx-target="<%= @id %>">Up</button>
+      </div>
+    </div>
+    """
+  end
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  def handle_event("switch", %{"switch" => "on"}, %{assigns: %{room: %{name: name}}} = socket) do
+    set_room_brightness(name, 100)
+
+    {:noreply, socket}
+  end
+
+  def handle_event("switch", %{"switch" => "off"}, %{assigns: %{room: %{name: name}}} = socket) do
+    set_room_brightness(name, 0)
+
+    {:noreply, socket}
+  end
+
+  def handle_event(
+        "dimmer",
+        %{"dimmer" => "up"},
+        %{
+          assigns: %{
+            room: %{
+              brightness: brightness,
+              name: name
+            }
+          }
+        } = socket
+      )
+      when brightness < 100 do
+    brightness = brightness + 10
+
+    set_room_brightness(name, brightness)
+    {:noreply, socket}
+  end
+
+  def handle_event(
+        "dimmer",
+        %{"dimmer" => "down"},
+        %{
+          assigns: %{
+            room: %{
+              brightness: brightness,
+              name: name
+            }
+          }
+        } = socket
+      )
+      when brightness > 0 do
+    brightness = brightness - 10
+
+    set_room_brightness(name, brightness)
+    {:noreply, socket}
+  end
+
+  def handle_event(_event, _value, socket) do
+    {:noreply, socket}
+  end
+
+  def update(%{room: room, id: id}, socket) do
+    socket =
+      socket
+      |> assign(:room, room)
+      |> assign(:id, id)
+
+    {:ok, socket}
+  end
+
+  defp set_room_brightness(name, brightness) do
+    send(
+      self(),
+      {__MODULE__, :set_brightness, %{room: name, brightness: brightness}}
+    )
+  end
+end
--- lib/light_web/live/room/index_live.ex
+++ lib/light_web/live/room/index_live.ex
@@ -1,15 +1,47 @@
 defmodule LightWeb.Room.IndexLive do
   use LightWeb, :live_view
+  alias LightWeb.Room.Component.ModalController
+
+  @rooms [
+    %{id: 1, name: "Kitchen", brightness: 100},
+    %{id: 2, name: "Sala", brightness: 100},
+    %{id: 3, name: "Bedroom 1", brightness: 100},
+    %{id: 4, name: "Bedroom 2", brightness: 100},
+    %{id: 5, name: "Hallway", brightness: 100}
+  ]
 
   def render(assigns) do
     Phoenix.View.render(LightWeb.RoomView, "index.html", assigns)
   end
 
-  def mount(params, session, socket) do
+  def mount(_params, _session, socket) do
+    socket =
+      socket
+      |> assign(:rooms, @rooms)
+
     {:ok, socket}
   end
 
   def handle_event(_event, _value, socket) do
     {:noreply, socket}
   end
+
+  def handle_info(
+        {ModalController, :set_brightness,
+         %{
+           room: selected_room,
+           brightness: brightness
+         }},
+        %{assigns: %{rooms: rooms}} = socket
+      ) do
+    rooms = Enum.map(rooms, &set_room_brightness(&1, selected_room, brightness))
+
+    {:noreply, assign(socket, :rooms, rooms)}
+  end
+
+  defp set_room_brightness(%{name: name} = room, selected_room, brightness)
+       when name == selected_room,
+       do: %{room | brightness: brightness}
+
+  defp set_room_brightness(room, _selected_room, _brightness), do: room
 end
